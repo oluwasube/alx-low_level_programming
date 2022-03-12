66186659a60fec3be1ba0bc49d6f0fe07a1a1bef
Oluwasube promise <oluwasube@outlook.com>
2022-03-12 12:35:42 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Prints the alphabetic
  *
- * Return: Always 0 (Success)
- */
-
+ * Return: Always (Success)
+ **/
 int main(void)
 {
-	char low;
+		char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+
 		putchar('\n');
-
-	return (0);
+		return (0);
 }

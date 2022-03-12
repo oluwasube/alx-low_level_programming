aac42073115a9c9b67f6b59e5f56474594f36746
Oluwasube promise <oluwasube@outlook.com>
2022-03-12 13:56:52 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 
+
 /**
  * main - Prints combination of numbers
  *
- * Return: Always (Success)
- **/
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int c, i;
+	int d;
 
-	for (c = '0'; c <= '9'; c++)
+	for (d = 0; d < 100; d++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
 		{
-			if (c < i)
-			{
-				putchar(c);
-				putchar(i);
-
-				if (c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
